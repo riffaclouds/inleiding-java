71772cdc0ef4a0f6ebb9a3c631bab234f12c5c39
riffaclouds <riffa@riffaclouds.com>
2016-01-31 04:43:34 +0100

    URI update.1

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -3,13 +3,9 @@ package h01;
 public class HalloRocDev {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
-		System.out.println("[Ubuntu Gnome15.10]!" );
+		System.out.println("1. [Ubuntu]!" );
 
-=======
-		System.out.println("[Ubuntu Gnome 15.10]!" );
->>>>>>> a564133c84960df0e9a315b09c72c3b2136c262b
 	}
 
 }
