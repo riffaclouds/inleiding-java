1143f3445e68f9553d05935954d18f3cd57fdc23
riffaclouds <riffa@riffaclouds.com>
2016-02-03 17:14:53 +0100

    Yoga > Decepticon

--- src/h2applets/Diagram.java
+++ src/h2applets/Diagram.java
@@ -12,12 +12,12 @@ public class Diagram extends Applet {
 	
 	public void paint(Graphics g){
 		//Voting Applet for the US elecetion 2016!
-		//1
+		//kandidaat 1
 		setBackground(Color.white);
 		g.setColor(Color.blue);
 		g.fillRect(10, 200, 60, 100);
 		g.drawString("Hillary Clinton", 5, 315);
-		//2
+		//kandidaat 2
 		g.setColor(Color.red);
 		g.fillRect(110, 200, 60, 100);
 		g.drawString("Bernie Sanders", 105, 315);
