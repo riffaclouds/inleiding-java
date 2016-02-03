503f73001c9d02bc643c47362bc1b3c4e1dc531e
riffaclouds <riffa@riffaclouds.com>
2016-02-03 16:21:06 +0100

    Yoga > Decepticon

---
+++ src/h2applets/Diagram.java
@@ -0,0 +1,27 @@
+package h2applets;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class Diagram extends Applet {
+	
+	public void init() {
+		setSize(400, 400);
+	}
+	
+	public void paint(Graphics g){
+		//1
+		setBackground(Color.white);
+		g.setColor(Color.blue);
+		g.fillRect(10, 200, 60, 100);
+		g.drawString("Hillary Clinton", 5, 315);
+		//2
+		g.setColor(Color.red);
+		g.fillRect(110, 200, 60, 100);
+		g.drawString("Bernie Sanders", 105, 315);
+		
+	}
+	
+
+}
