b2f3757b3745052d3ba0a394555dfdf939a76825
riffaclouds <riffa@riffaclouds.com>
2016-02-04 17:09:44 +0100

    Yoga > Decepticon

--- src/h2applets/Diagram.java
+++ src/h2applets/Diagram.java
@@ -1,17 +1,41 @@
 package h2applets;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.applet.*;
 
 
 public class Diagram extends Applet {
+	Button knop;
+	Button vote;
+	String schermtekst;
+	TextField tekstvak;
+	
 	
 	public void init() {
-		setSize(400, 400);
+		//grote(x, y) van de applet window bepalen
+		//configure the size of the applet window
+		setSize(600, 600);
+		
+		//string object initialiseren
+		schermtekst = "doet deze knop wel iets";
+		
+		tekstvak = new TextField("klik op me", 20);
+		add(tekstvak);
+		
+		knop = new Button("click for critical mass Lvl1");
+		KnopListener kl = new KnopListener();
+        knop.addActionListener( kl );
+		add(knop);
+		
 	}
 	
 	public void paint(Graphics g){
 		//Voting Applet for the US elecetion 2016!
+		
+		
+		
+		
 		//kandidaat 1
 		setBackground(Color.white);
 		g.setColor(Color.blue);
@@ -21,7 +45,21 @@ public class Diagram extends Applet {
 		g.setColor(Color.red);
 		g.fillRect(110, 200, 60, 100);
 		g.drawString("Bernie Sanders", 105, 315);
+		g.setColor(Color.black);
+		g.drawLine(170, 35, 170, 320);
 		
+		g.drawString(schermtekst, 170, 50);
+		
+	}
+	
+	class KnopListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			
+			schermtekst = "ja, ik kom uit een inwendige klasse!";
+					
+			//refesh the screen, making the change visible
+			repaint();
+		}
 	}
 	
 
