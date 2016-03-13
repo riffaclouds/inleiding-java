9627259711390be8f4b1fce1d00cc7fc116a394a
riffaclouds <riffa@riffaclouds.com>
2016-03-13 01:12:19 +0100

    Decepticon < Yoga  (through bash)

---
+++ src/h2applets/Tabel.java
@@ -0,0 +1,17 @@
+package h2applets;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+
+public class Tabel extends Applet {
+	double salaris[];
+	
+	public void init(){
+		
+	}
+	
+	public void paint(Graphics G){
+		
+	}
+}
