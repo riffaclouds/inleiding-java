39cf6b0a9f5d4d39352f32c9c5b718520ab28098
riffaclouds <riffa@riffaclouds.com>
2016-04-09 20:51:10 +0200

    win test

--- src/h2applets/Tabel.java
+++ src/h2applets/Tabel.java
@@ -5,13 +5,29 @@ import java.awt.event.*;
 import java.applet.*;
 
 public class Tabel extends Applet {
-	double salaris[];
+	double[] salaris;
+	String[] earthling;
 	
 	public void init(){
-		
+		setBackground(Color.white);
+		salaris = new double[6];
+		earthling = new String[] {" Suzi ", " pipi ", " poepie "};
+
+
 	}
 	
-	public void paint(Graphics G){
+	public void paint(Graphics g){
+		int x = 10;
+		int y = 15;
+
+		/*
+		g.drawString(earthling[0] + earthling[1] + earthling[2], x, y);
+		y+=15;
+		g.drawString(earthling[1], x, y);
+		y+=15;
+		g.drawString(earthling[2], x, y);
+		*/
+
 		
 	}
 }
