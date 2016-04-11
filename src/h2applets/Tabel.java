8b2cbdab8cc2fe18529616550245647be1f3332c
riffaclouds <riffa@riffaclouds.com>
2016-04-11 23:56:22 +0200

    win test 2

--- src/h2applets/Tabel.java
+++ src/h2applets/Tabel.java
@@ -5,13 +5,20 @@ import java.awt.event.*;
 import java.applet.*;
 
 public class Tabel extends Applet {
-	double[] salaris;
+	long[] salaris;
 	String[] earthling;
 	
 	public void init(){
 		setBackground(Color.white);
-		salaris = new double[6];
-		earthling = new String[] {" Suzi ", " pipi ", " poepie "};
+		salaris = new long[10];
+		earthling = new String[] {" Suzi ", " piepi ", " poepie "};
+		/*salaris[0] = 3000;
+		salaris[1] = 30000;
+		salaris[2] = 12500000; */
+
+		for (int teller = 0; teller < salaris.length; teller ++) {
+			salaris[teller] = 100 * teller + 100;
+		}
 
 
 	}
@@ -20,12 +27,14 @@ public class Tabel extends Applet {
 		int x = 10;
 		int y = 15;
 
+		//print out suzi's name
+		//g.drawString(earthling[0] + earthling[1] + earthling[2], x, y);
+		//g.drawString("" + salaris[0] + " " + salaris[1] + " " +  salaris[2], x, y);
 
-		g.drawString(earthling[0] + earthling[1] + earthling[2], x, y);
-		y+=15;
-		g.drawString(earthling[1], x, y);
-		y+=15;
-		g.drawString(earthling[2], x, y);
+		for (int teller = 0; teller < salaris.length; teller ++) {
+			g.drawString("" + salaris[teller], 10, 20 * teller + 20);
+		}
+
 
 
 
